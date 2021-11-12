0d46efac16f4eae3e09032ed2d2c9637638d748d
HachiyaNoe <noe.hachiya@rakus-partners.co.jp>
2021-11-12 13:34:16 +0900

    ex03-e04 finish

--- src/test/java/junit/tutorial/ex03/e04/AuthenticationTest.java
+++ src/test/java/junit/tutorial/ex03/e04/AuthenticationTest.java
@@ -1,12 +1,23 @@
 package junit.tutorial.ex03.e04;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 
 class AuthenticationTest {
 
@@ -26,6 +37,25 @@ class AuthenticationTest {
 	void tearDown() throws Exception {
 	}
 
+	// テスト対象で使用するクラスに対してつけるアノテーション
+
+	 // @Spy   一部のメソッドだけモックにしたいときはこれを定義
+	   
+	 @Mock
+	 private AccountDao dao;
 	
+	 @InjectMocks  // テスト対象のクラスに対してつけるアノテーション
+	 Authentication authentication = new Authentication();
+	 
+	 @Test
+		@DisplayName("AccountDaoがnullを返す場合 -> authenticateメソッドはnullを返す")
+		void test1()  {
+		 doReturn(null)
+		  .when(dao).findOrNull("1");
+		 
+		 assertNull(authentication.authenticate("1", "")); //password部分は任意＝nullでも良いしなんでも
+
+			
+		}
 
 }
